9fb886e85a86cf51a8267a2fbcbde71c5743a1fb
nasz_login_do_github <nasz_email_do_github>
2020-03-16 17:25:19 +0100

    class calculator with class application

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Application.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.calculator;
+
+    class Application {
+        public static void main (String[]args) {
+            Calculator calculator = new Calculator();
+            int resultAdd = calculator.addAToB(6,8);
+            int resultSubstract = calculator.substractAFromB(87,54);
+
+            System.out.println("Result Add "+ resultAdd);
+            System.out.println("Result Substract "+ resultSubstract);
+        }
+    }
--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/calculator.java
@@ -8,14 +8,3 @@ class Calculator {
         return a - b;
     }
 }
-
-class Application {
-    public static void main (String[]args) {
-        Calculator calculator = new Calculator();
-        int resultAdd = calculator.addAToB(6,8);
-        int resultSubstract = calculator.substractAFromB(87,54);
-
-        System.out.println("Result Add "+ resultAdd);
-        System.out.println("Result Substract "+ resultSubstract);
-    }
-}
