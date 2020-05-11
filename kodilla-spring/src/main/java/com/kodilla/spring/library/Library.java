d4e9549944f8348d2b52d58b4fa42d4d289b9518
nasz_login_do_github <nasz_email_do_github>
2020-05-11 13:47:35 +0200

    14.2

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -1,5 +1,6 @@
 package com.kodilla.spring.library;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,14 +9,14 @@ import java.util.List;
 @Service
 public final class Library {
     private final List<String> books = new ArrayList<>();
-    private final LibraryDbController libraryDbController;
 
-    public Library(final LibraryDbController libraryDbController) {
-        this.libraryDbController = libraryDbController;
-    }
+    @Autowired
+    private LibraryDbController libraryDbController;
+
     public void saveToDb() {
         libraryDbController.saveData();
     }
+
     public void loadFromDb() {
         libraryDbController.loadData();
     }
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,64 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+
+    @Test
+    public void add() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.add(3,3);
+        //Then
+        Assert.assertEquals(6.0, result,0.001);
+
+    }
+
+    @Test
+    public void sub() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.sub(3,3);
+        //Then
+        Assert.assertEquals(0.0, result,0.001);
+    }
+
+    @Test
+    public void mul() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.mul(3,3);
+        //Then
+        Assert.assertEquals(9.0, result,0.001);
+    }
+
+    @Test
+    public void div() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.div(3,3);
+        //Then
+        Assert.assertEquals(1.0, result,0.001);
+    }
+}
+
