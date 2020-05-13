2cc04ce32ae82b42f33ad08d55b44337e002ae4a
nasz_login_do_github <nasz_email_do_github>
2020-05-13 07:45:13 +0200

    15.2

--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -10,9 +10,9 @@ public class LibraryTestSuite {
     public void testGetBooks () throws CloneNotSupportedException {
         //Given
         Library library = new Library("My library");
-        Book book1 = new Book("Gwiazd naszych wina", "Jonh Green", LocalDate.of(2012, 01, 01));
-        Book book2 = new Book("Chłopiec w pasiastej piżamie", "John Boyne", LocalDate.of(2006, 01, 01));
-        Book book3 = new Book("Opowieść Podręcznej", "Margaret Atwood", LocalDate.of(1985, 01, 01));
+        Book book1 = new Book("Gwiazd naszych wina", "Jonh Green", LocalDate.of(2012, 1, 1));
+        Book book2 = new Book("Chłopiec w pasiastej piżamie", "John Boyne", LocalDate.of(2006, 1, 1));
+        Book book3 = new Book("Opowieść Podręcznej", "Margaret Atwood", LocalDate.of(1985, 1, 1));
         library.getBooks().add(book1);
         library.getBooks().add(book2);
         library.getBooks().add(book3);
@@ -26,7 +26,7 @@ public class LibraryTestSuite {
         deepClonedLibrary.setName("Your deep library");
 
         //When
-        library.getBooks().add(new Book("Cierpienia młodego Wertera","Johan Wolfgang von Goethe", LocalDate.of(1774,01,01)));
+        library.getBooks().add(new Book("Cierpienia młodego Wertera","Johan Wolfgang von Goethe", LocalDate.of(1774,1,1)));
 
         //Then
         Assert.assertEquals(4, library.getBooks().size());
