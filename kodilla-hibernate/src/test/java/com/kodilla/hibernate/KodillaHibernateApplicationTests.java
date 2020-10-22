a67e99454e76e45301841962123af40f4bab1513
nasz_login_do_github <nasz_email_do_github>
2020-10-22 20:46:17 +0200

    17.2

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
@@ -1,4 +1,11 @@
 package com.kodilla.hibernate;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class KodillaHibernateApplicationTests {
+@SpringBootApplication
+public class KodillaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaHibernateApplication.class, args);
+    }
 }
